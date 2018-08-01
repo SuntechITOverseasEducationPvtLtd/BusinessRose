2baf701ba2fa0fdec325090963309771dc436b19
Venkata Sandeep Kanni <kv.90948@nagarjuna.com>
2018-08-01 17:09:03 +0530

    First commit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
-use App\Notifications\SignupActivate;  
+use App\Notifications\SignupActivate;   
 use Carbon\Carbon;
 
 

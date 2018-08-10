3d8c4f4f5988ff354625e0ba4eadc086549e708f
Venkata Sandeep Kanni <kv.90948@nagarjuna.com>
2018-08-10 14:52:36 +0530

    changes on 10-08-2018

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -21,25 +21,35 @@ use DB;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Collection;
 
-class DashboardController extends Controller
+class MemberController extends Controller
 {
 	public function __construct()
 	{
-		$this->module_view_folder 		= "admin.dashboard";
+		$this->module_view_folder 		= "admin.members";
 		$this->admin_url_path     		= url(config('app.project.admin_panel_slug'));
 	}
    
-    public function dashboard()
+    public function memberActivations()
     {    	
     	$arr_view_data['cats'] = "1";
     	$arr_view_data['sub_cats'] = "2";
-    	return view($this->module_view_folder.'.home',$arr_view_data);
+    	return view($this->module_view_folder.'.activations',$arr_view_data);
     }
 
-    public function priceList()
+    public function memberProfiles()
     {       
-        $arr_view_data['priceList'] = "1";
-        return view('admin.prices.priceList',$arr_view_data);
+        $arr_view_data['cats'] = "1";
+        $arr_view_data['sub_cats'] = "2";
+        return view($this->module_view_folder.'.Profiles',$arr_view_data);
     }
+
+    public function memberPurchases()
+    {       
+        $arr_view_data['cats'] = "1";
+        $arr_view_data['sub_cats'] = "2";
+        return view($this->module_view_folder.'.purchases',$arr_view_data);
+    }
+
+    
 
 }
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,12 @@ Route::group(['prefix' => $admin_path,'middleware'=>['admin'],'namespace'=>'Admi
 	Route::post('process_login', 'AuthController@process_login');
 	Route::get('logout', 'AuthController@logout');
 	Route::get('dashboard', 'DashboardController@dashboard');
+	Route::get('price_list', 'DashboardController@priceList');
+	Route::get('member_activations', 'MemberController@memberActivations');
+	Route::get('investor_activations', 'InvestorController@investorActivations');
+	Route::get('member_purchases', 'MemberController@memberPurchases');
+	Route::get('investor_purchases', 'InvestorController@investorPurchases');
+	Route::get('member_profiles', 'MemberController@memberProfiles');
+	Route::get('investor_profiles', 'InvestorController@investorProfiles');
 	
 });

0785fe9d713f510c9eab30c3fac5a8034bb73a5f
Venkata Sandeep Kanni <kv.90948@nagarjuna.com>
2018-08-19 10:31:48 +0530

    changes

--- resources/views/admin/layout/_footer.blade.php
+++ resources/views/admin/layout/_footer.blade.php
@@ -1,5 +1,5 @@
 </div>
-<!-- Footer -->
+
 <div class="navbar navbar-expand-lg navbar-light">
 	<div class="text-center d-lg-none w-100">
 		<button type="button" class="navbar-toggler dropdown-toggle" data-toggle="collapse" data-target="#navbar-footer">
@@ -20,5 +20,3 @@
 		</ul>
 	</div>
 </div>
-
-<!-- /footer -->
